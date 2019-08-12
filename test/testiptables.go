f5c7a94507785f0d338fd94c73c55532075a79ae
rickey.liao <micliao@gmail.com>
2019-08-12 10:22:39 +0800

    add to repo

--- test/testiptables.go
+++ test/testiptables.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"github.com/coreos/go-iptables/iptables"
-	"fmt"
+	"github.com/kprc/flowsharectrl/config"
+	"github.com/kprc/flowsharectrl/control"
 )
 
 func main()  {
@@ -13,23 +13,36 @@ func main()  {
 	//
 	//dli.Print()
 
-	ipt,_:=iptables.New()
+	//ipt,_:=iptables.New()
+	////
+	////fmt.Println(ipt.ListChains("filter"))
 	//
-	//fmt.Println(ipt.ListChains("filter"))
-
-	//r,_:=ipt.ListWithCounters("filter","FORWARD")
+	////r,_:=ipt.ListWithCounters("filter","FORWARD")
+	////
+	////fmt.Println(r)
+	////
+	//s,_:=ipt.StructuredStats("filter","FORWARD")
 	//
-	//fmt.Println(r)
+	//fmt.Println(s)
 	//
-	s,_:=ipt.StructuredStats("filter","FORWARD")
-
-	fmt.Println(s)
-
-	l,_:=ipt.List("filter","FORWARD")
-
-	fmt.Println(l)
+	//l,_:=ipt.List("filter","FORWARD")
+	//
+	//fmt.Println(l)
 
 	//control.GetFCListInst().Accept("abcdefghijklmnopqrst","11:22:33:44:55")
+
+	fclc:=&config.FCLConfig{}
+	if _,err:=fclc.Load();err!=nil{
+		fclc.InitFCLConfig("","",true)
+		fclc.Save()
+	}
+
+	fcl:=control.GetFCListInst()
+	fcl.Accept("aaa","mac","ip")
+	fcl.Deny("aaa")
+	fcl.GetDownBytes("aaa")
+
+
 
 }
 
